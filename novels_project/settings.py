446aff052dd13c440add7b1fd2a5977059d39483
M Noman Ahsan <miannoman075@gmail.com>
2025-11-03 15:30:59 +0500

    new

--- novels_project/settings.py
+++ novels_project/settings.py
@@ -112,6 +112,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
